77520a0a4f25b58747e584305ecfc82a8e21fc55
mbbirkel <mbbirkel@stud.ntnu.no>
2023-02-16 16:32:52 +0100

    wip cloud script. Clean up cloud shader graph

--- Assets/Script/GenerateCloudsMap.cs
+++ Assets/Script/GenerateCloudsMap.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-
-
 public class CloudMap {
     
     private Texture2D texture;
@@ -14,11 +12,11 @@ public class CloudMap {
         this.timeSeconds = t;
     }
 
-    public Texture2D getTexture(){
+    public Texture2D GetTexture(){
         return texture;
     }
 
-    public int getTime(){
+    public int GetTime(){
         return timeSeconds;
     }
 }
@@ -29,22 +27,52 @@ public class GenerateCloudsMap : MonoBehaviour
     public GameObject clouds;
     public string imageDirectory = "ImageData/Bergen/Emission";
 
-
-    public int index = 0;
-
-    private int prevIndex = 0;
-
+    private int indexMin = 0;
+    private int indexMax = 1;
     private Renderer cloudsRenderer;
 
-    private List<CloudMap> CloudMaps;
+    // All textures are loaded into this list, and used at runtime.
+    private List<CloudMap> cloudMaps;
+
+    private void SetMinMap(int index){
+        cloudsRenderer.material.SetTexture("_ColorMapMin", cloudMaps[index].GetTexture());
+    }
+
+    private void SetMaxMap(int index){
+        cloudsRenderer.material.SetTexture("_ColorMapMax", cloudMaps[index].GetTexture());
+    }
+
+    private void SetMaps(){
+        SetMinMap(indexMin);
+        SetMaxMap(indexMax);
+    }
+
+    public void IncrementTime(){
+        if (indexMax >= cloudMaps.Count-1){
+            throw new System.NullReferenceException("Index out of range");
+        }
+        indexMin++;
+        indexMax++;
+        SetMaps();
+    }
+
+    public void DecrementTime(){
+        if(indexMin <= 0 && indexMax != indexMin){
+            throw new System.IndexOutOfRangeException("Index out of range");
+        }
+        indexMin--;
+        indexMax--;
+        SetMaps();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         cloudsRenderer = clouds.GetComponent<Renderer>();
-        CloudMaps = new List<CloudMap>();
+        cloudMaps = new List<CloudMap>();
         DirectoryInfo info = new DirectoryInfo(imageDirectory);
         FileInfo [] fileInfo = info.GetFiles();
+
         // Load all png's from folder into the CloudMaps list.
         foreach(FileInfo file in fileInfo){
             if(file.Extension.Equals(".png") || file.Extension.Equals(".PNG")){
@@ -56,20 +84,29 @@ public class GenerateCloudsMap : MonoBehaviour
                 Debug.Log("Filename: " + file.Name);
                 int seconds = int.Parse(file.Name.Split('.')[0]);
                 CloudMap cm = new CloudMap(texture, seconds);
-                CloudMaps.Add(cm);
+                cloudMaps.Add(cm);
             }
         }
-        // Sort mayb.
 
-        cloudsRenderer.material.SetTexture("_ColorMap", CloudMaps[index].getTexture());
+        if(cloudMaps.Count <= 0){
+            throw new System.IndexOutOfRangeException("No textures found at: " + imageDirectory);
+        }
+
+        if(cloudMaps.Count == 1){
+            indexMax = 0;
+        }
+
+        // Sort by time
+        cloudMaps.Sort((c1,c2) => {
+            return c1.GetTime() -c2.GetTime();
+        });
+
+        SetMaps();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(index != prevIndex){
-            cloudsRenderer.material.SetTexture("_ColorMap", CloudMaps[index].getTexture());
-            prevIndex = index;
-        }
+
     }
 }
