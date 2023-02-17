610cab2f620950d34a9a612bea6d28fdde3b1f6c
mbbirkel <mbbirkel@stud.ntnu.no>
2023-02-17 14:18:38 +0100

    add variable to toggle rad/building scripts

--- Assets/ShowConstructionDataScript.cs
+++ Assets/ShowConstructionDataScript.cs
@@ -8,16 +8,19 @@ public class ShowConstructionDataScript : MonoBehaviour
     public GameObject Cube; // Reference to the cube object
     public Toggle ConstructionDataToggle; // Reference to the UI toggle object
 
+    public bool showConstruction = true;
+
     // Start is called before the first frame update
     void Start()
     {
         // Set the toggle to match the initial state of the cube object
-        ConstructionDataToggle.isOn = Cube.activeSelf;
+        ConstructionDataToggle.isOn = showConstruction;
     }
 
     // Update is called once per frame
     void Update()
     {
+        ConstructionDataToggle.isOn = showConstruction;
         // Check if the toggle value has changed
         if (ConstructionDataToggle.isOn != Cube.activeSelf)
         {
--- Assets/ShowRadiationScript.cs
+++ Assets/ShowRadiationScript.cs
@@ -7,17 +7,19 @@ public class ShowRadiationScript : MonoBehaviour
 {
     public GameObject RadiationCube; // Reference to the cube object
     public Toggle RadiationToggle; // Reference to the UI toggle object
+    public bool showRadiation = true;
 
     // Start is called before the first frame update
     void Start()
     {
         // Set the toggle to match the initial state of the cube object
-        RadiationToggle.isOn = RadiationCube.activeSelf;
+        RadiationToggle.isOn = showRadiation;
     }
 
     // Update is called once per frame
     void Update()
     {
+        RadiationToggle.isOn = showRadiation;
         // Check if the toggle value has changed
         if (RadiationToggle.isOn != RadiationCube.activeSelf)
         {
