beb21adba8e76885d4b6ab17c050ebc6385e2153
egilhage <egilhage@stud.ntnu.no>
2023-02-21 16:27:26 +0100

    deleted map navigation sliders. changed togglescript public variable to private

--- Assets/ToggleObjectScript.cs
+++ Assets/ToggleObjectScript.cs
@@ -6,22 +6,22 @@ using UnityEngine.UI;
 public class ToggleObjectScript : MonoBehaviour
 {
     public GameObject objectToToggle; // Reference to the cube object
-    public Toggle toggle; // Reference to the UI toggle object
+    public Toggle toggleUI; // Reference to the UI toggle object
 
-    public bool showObject = true;
+    private bool showObject = true;
 
     // Start is called before the first frame update
     void Start()
     {
         // Set the toggle to match the initial state of the cube object
-        toggle.isOn = showObject;
+        toggleUI.isOn = showObject;
         objectToToggle.SetActive(showObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        showObject = toggle.isOn;
+        showObject = toggleUI.isOn;
         // Check if the toggle value has changed
         if (showObject != objectToToggle.activeSelf)
         {
