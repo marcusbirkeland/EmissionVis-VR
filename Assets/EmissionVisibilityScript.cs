9c7e63724f170fb16aa54ac49cdb826d33922853
Joostend <jens.oostendorp@live.com>
2023-02-22 01:13:58 +0100

    fix UI interactions (opacity slider + object toggling)

--- Assets/EmissionVisibilityScript.cs
+++ Assets/EmissionVisibilityScript.cs
@@ -6,17 +6,24 @@ using UnityEngine.UI;
 public class EmissionVisibilityScript : MonoBehaviour
 {
 
-    private  GameObject currentGameObject;
+    private GameObject currentGameObject;
     public float alpha = 1f;
     //Get current material
-    private Material currentMat;
+    private List<Material> currentMats = new List<Material>();
 
     // Start is called before the first frame update
     void Start()
     {
-        currentGameObject = gameObject;
-        currentMat = currentGameObject.GetComponent<Renderer>().material;
-
+        currentGameObject = this.gameObject;
+        
+        LOD[] lods = currentGameObject.GetComponent<LODGroup>().GetLODs();
+        foreach (LOD lod in lods)
+        {
+            foreach (Renderer ren in lod.renderers)
+            {
+                currentMats.Add(ren.material);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -25,14 +32,19 @@ public class EmissionVisibilityScript : MonoBehaviour
 
     }
 
-    void ChangeAlpha(Material mat, float alphaVal)
+    void ChangeAlpha(float alphaVal)
     {
-        mat.SetFloat("_Opacity", alphaVal);
+        // Ideally, only the current loaded LOD would have to be altered. 
+        // There is no clear way to extract this information.
+        foreach (Material mat in currentMats)
+        {
+            mat.SetFloat("_Opacity", alphaVal);
+        }
     }
 
     public void ChangeAlphaOnValueChange(Slider slider)
     {
-        ChangeAlpha(currentMat, slider.value);
+        ChangeAlpha(slider.value);
     }
     
 }
