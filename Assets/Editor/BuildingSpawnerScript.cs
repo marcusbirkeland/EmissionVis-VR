c1ec4e24a275a9fc6b694fa96fe5a644a054b2b5
Joostend <jens.oostendorp@live.com>
2023-03-19 00:54:16 +0100

    ray casting for building placement to find correct altitude

--- Assets/Editor/BuildingSpawnerScript.cs
+++ Assets/Editor/BuildingSpawnerScript.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using UnityEditor;
 using Microsoft.Maps.Unity;
 using Microsoft.Geospatial;
+using System;
 
 public class BuildingSpawner : EditorWindow
 {
@@ -15,12 +16,13 @@ public class BuildingSpawner : EditorWindow
     public string elevationDataPath="Assets/Resources/Bergen/Elevation/elevation.csv";  
     //public Vector3 positionOffset;
 
-    public float xAngle=0, yAngle=-90, zAngle=0;
-
     public float altitudeOffset = -250;
 
     public double latitude = 60.35954907032411;
     public double longitude = 5.314180944287559;
+
+    // How many meters on the map go into one coordinate unit in Unity world Space. 
+    double metersPerUnit; 
 
     public int data_y_index = 0;
     public int data_x_index = 1;
@@ -44,9 +46,6 @@ public class BuildingSpawner : EditorWindow
         GUILayout.Label("Building generator!");
         //textureFolderPath = EditorGUILayout.TextField("Path to textures folder", textureFolderPath);
         dataPath = EditorGUILayout.TextField("Path to building data: ", dataPath);
-        xAngle = EditorGUILayout.FloatField("x-angle", xAngle);
-        yAngle = EditorGUILayout.FloatField("y-angle", yAngle);
-        zAngle = EditorGUILayout.FloatField("z-angle", zAngle);
 
         data_y_index = EditorGUILayout.IntField("y-index in data", data_y_index);
         data_x_index = EditorGUILayout.IntField("x-index in data", data_x_index);
@@ -66,6 +65,13 @@ public class BuildingSpawner : EditorWindow
         {
             SpawnBuildings();
         }
+
+        GUIStyle textStyle = GUI.skin.GetStyle("PR TextField");
+        textStyle.wordWrap = true;
+        EditorGUILayout.TextArea(@"NOTE:
+        1) At lower zoom levels (zoomed out) the altitude of the ray cast hit becomes inaccurate (zoomLevel < ~12). Try to zoom in as far as possible, BUT
+        2) at very high zoom levels (zoomed in) the ray casts move outside of the map collider and won't intersect with it. Make sure the map is zoomed out sufficiently enough to include the entire area that will be covered with buildings. ",
+        textStyle);
     }
 
 
@@ -75,7 +81,9 @@ public class BuildingSpawner : EditorWindow
 
         // Instantiate new GameObject as child of map.
         buildings = new GameObject("Map Buildings");
-        buildings.transform.parent = map.transform;
+
+        // SetParent() is the same as the .parent property except that it also lets the Transform keep its local orientation rather than its global orientation. 
+        buildings.transform.SetParent(map.transform, false);
 
         System.IO.StreamReader sr = new System.IO.StreamReader(dataPath);
         if(sr.Peek() < 0){
@@ -100,8 +108,21 @@ public class BuildingSpawner : EditorWindow
         long currentLine = 1;
         long counter = 0; // Counter variable to prevent writing to progress bar too often.
 
+
+        // Calculations for finding the number of meters that go into 1 Unity coordinate unit
+        Vector3 worldSpacePin = MapRendererTransformExtensions.TransformLatLonAltToWorldPoint(map.GetComponent<MapRenderer>(), new LatLonAlt(latitude, longitude, 0.0));
+        // (map.GetComponent<MapRenderer>().MapDimension.x / 2) is the amount of world space units from the center of the map to the edge. 
+        Vector3 destinationWorld = worldSpacePin + map.transform.right * (map.GetComponent<MapRenderer>().MapDimension.x / 2);
+        LatLon destination = MapRendererTransformExtensions.TransformWorldPointToLatLon(map.GetComponent<MapRenderer>(), destinationWorld);
+        double angle = destination.LongitudeInDegrees - longitude;
+        double metersPerDegree = MapRendererTransformExtensions.EquatorialCircumferenceInWgs84Meters / 360.0;
+        metersPerUnit = metersPerDegree * angle * Math.Cos((Math.PI / 180) * latitude);
+        // dividing by only the x scale (world space) doesn't matter, as the map is guaranteed to be scaled equally on all axis
+        metersPerUnit = metersPerUnit / map.transform.lossyScale.x; 
+
+
         // Read file line by line
-        while(sr.Peek() >= 0){
+        while (sr.Peek() >= 0){
             string line = sr.ReadLine();
             //Debug.Log("line: " + line);
 
@@ -120,13 +141,13 @@ public class BuildingSpawner : EditorWindow
             counter++;
         }
 
+
+
         Debug.Log("Finished reading file. Closing..");
         sr.Close();
         EditorUtility.ClearProgressBar();
         Debug.Log("Stream successfully closed.");
         Debug.Log("Spawned " + buildings.transform.childCount + " buildings.");
-
-        buildings.transform.Rotate(xAngle, yAngle, zAngle);
 
         //Setup map pin component
         MapPin mapPin = buildings.AddComponent<MapPin>();
@@ -135,6 +156,9 @@ public class BuildingSpawner : EditorWindow
         mapPin.Altitude = altitudeOffset;
         mapPin.enabled = true;
 
+        // Ellipsoid: "The altitude reference is based on an ellipsoid which is a mathematical approximation of the shape of the Earth."
+        // Meaning the map pin is located at "sea level" rather than at the terrain height of the LatLon location of the pin
+        mapPin.AltitudeReference = Microsoft.Geospatial.AltitudeReference.Ellipsoid;
     }
 
     /* Parse building data from a data-line, and handle input.*/
@@ -143,7 +167,7 @@ public class BuildingSpawner : EditorWindow
         string x = data[data_x_index];
         string height = data[data_height_index];
 
-        Debug.Log("x: " + x + " y: " + y + " height: " + height);
+        //Debug.Log("x: " + x + " y: " + y + " height: " + height);
         // Spawn building if the current line has a height-property.
         if(height != null && height != "" && height != " "){
             SpawnBuilding(
@@ -162,27 +186,54 @@ public class BuildingSpawner : EditorWindow
 
         //TODO: Throw error if x and y values mismatch between building-data and elevation-data.
 
+        // Elevation data from dataset:
+        /*
         float y = float.Parse(
             elevationString[data_height_index] ,
             CultureInfo.InvariantCulture.NumberFormat
         );
-
         Vector3 pos = new Vector3(x, y, z);
+        */
+        
+
+        // Elevation data from ray casting:
+        Vector3 worldSpacePin = MapRendererTransformExtensions.TransformLatLonAltToWorldPoint(map.GetComponent<MapRenderer>(), new LatLonAlt(latitude, longitude, 0.0));
+
+        // Note: Ray casting can only be done in world space, meaning all coordinates have to be converted from local to world space 
+        // before ray casting, and back again afterwards.
+        // x and z coordinates are provided in meters, which must be converted to Unity coordinate units through division by metersPerUnit.
+        Vector3 origin = worldSpacePin + map.transform.right * (x / (float)metersPerUnit) + map.transform.forward * (z / (float)metersPerUnit);
+
+        // Place all points in world space some (10 for example) units away from the top of the map when ray casting towards the map.
+        Vector3 originOffset = origin + map.transform.up * (10.0f * map.transform.lossyScale.y);
+
+        // (map.transform.up * -1) will point towards the "top" (side with the texture) of the map from a point "origin",
+        // if "origin" is placed at a positive multiple of map.transform.up away from the map. 
+        Ray ray = new Ray(originOffset, map.transform.up * -1);
+
+        MapRendererRaycastHit hitInfo;
+        map.GetComponent<MapRenderer>().Raycast(ray, out hitInfo);
+
+        // Transform from world space to local space
+        Vector3 pos = map.transform.InverseTransformVector((hitInfo.Point - worldSpacePin)) * (float)metersPerUnit * map.transform.lossyScale.x;
+
+
+
         GameObject building = Instantiate(smallBuilding, buildings.transform, false);
         building.name = name;
-        building.transform.position += pos;
+        building.transform.localPosition += pos;
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
