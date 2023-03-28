2b6afe09a6cba8e230fcc07dc0c0d3583ba4698b
Joostend <jens.oostendorp@live.com>
2023-03-28 20:05:21 +0200

    WIP further work on scene change and full scale XR

--- Assets/Script/ChangeScene.cs
+++ Assets/Script/ChangeScene.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.Universal;
 
 public class ChangeScene : MonoBehaviour
 {
-    public float loadProgress = 0.0f;
+    public string sceneName;
     private AsyncOperation loadingOperation;
+    public UniversalRenderPipelineAsset pipeline;
 
-    public void ChangeToScene(string sceneName)
+    public void ChangeToScene()
     {
 
         loadingOperation = SceneManager.LoadSceneAsync(sceneName);
         enabled = true;
+        loadingOperation.completed += Test;
+        // pipeline.mainLightRenderingMode = LightRenderingMode.PerPixel; <- IS READ ONLY
+    }
 
+    void Test(AsyncOperation obj)
+    {
+        Debug.Log("FINISHED!");
     }
 
     // Start is called before the first frame update
@@ -25,13 +33,13 @@ public class ChangeScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        loadProgress = loadingOperation.progress;
-        Debug.Log(loadProgress);
+        /*
+        Debug.Log(loadingOperation.progress);
         if (loadingOperation.isDone)
         {
             Debug.Log("FINISHED");
             // TODO: change lighting here?
-        }
+        }*/
     }
 
 }
